462034a5c3a74f031928a91cf5509297c48096cf
twodollars123 <twodollars.no2002@gmail.com>
2023-06-04 21:41:44 +0700

    Them 4 testcase va ham tao chuoi ngau nhien

--- src/test/java/demoTest/testNG.java
+++ src/test/java/demoTest/testNG.java
@@ -5,13 +5,30 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.Test;
 import org.testng.Assert;
 
 import java.util.concurrent.TimeUnit;
-
+import java.nio.charset.Charset;
+import java.util.Random;
 public class testNG {
     WebDriver driver;
+
+
+
+    public String randomAlphaNumeric() {
+        int len = 6; // Độ dài của String random
+        byte[] array = new byte[len];
+        new Random().nextBytes(array);
+        String generatedString = new String(array, Charset.forName("UTF-8"));
+        return generatedString;
+    }
+
+
+
 
     @Test(priority = 1)
     public void Test01(){
@@ -28,6 +45,45 @@ public class testNG {
 
     @Test(priority = 2)
     public void Test02(){
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        driver.get("http://localhost:3000/");
+
+        driver.quit();
+    }
+
+    @Test(priority = 3)
+    public void Test03(){
+        WebDriverManager.safaridriver().setup();
+        driver = new SafariDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        driver.get("http://localhost:3000/");
+
+        driver.quit();
+    }
+
+    @Test(priority = 4)
+    public void Test04(){
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        driver.get("http://localhost:3000/");
+
+        driver.quit();
+    }
+
+    @Test(priority = 5)
+    public void Test05(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
 
@@ -60,7 +116,7 @@ public class testNG {
         driver.quit();
     }
 
-    @Test(priority = 3)
+    @Test(priority = 6)
     public void TestLoginFailure(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
@@ -84,6 +140,46 @@ public class testNG {
         btnLogin.click();
 
         WebElement successToast = driver.findElement(By.className("Toastify__toast--error"));
+        waitForElementToBeDisplayed(successToast);
+
+        // Kiểm tra xem thông báo có hiển thị thành công hay không
+        Assert.assertTrue(successToast.isDisplayed());
+
+
+
+        driver.quit();
+    }
+
+    @Test(priority = 7)
+    public void TestSignupSuccessful(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        driver.get("http://localhost:3000/signup");
+        String ramdomString = randomAlphaNumeric();
+
+        WebElement email = driver.findElement(By.id("email"));
+        email.clear();
+        email.sendKeys(ramdomString);
+
+        WebElement username = driver.findElement(By.id("user__name"));
+        username.clear();
+        username.sendKeys(ramdomString);
+
+
+        WebElement password = driver.findElement(By.id("password"));
+        password.clear();
+
+        password.sendKeys(ramdomString);
+
+
+        WebElement btnLogin = driver.findElement(By.id("btn__signup"));
+        btnLogin.click();
+
+        WebElement successToast = driver.findElement(By.className("Toastify__toast--success"));
         waitForElementToBeDisplayed(successToast);
 
         // Kiểm tra xem thông báo có hiển thị thành công hay không
